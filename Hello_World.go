8c7685217df33932f5db4fa57bff8df16c235ddb
VKNarra4774 <116201437+VKNarra4774@users.noreply.github.com>
2022-12-14 15:44:19 -0600

    Add files via upload

---
+++ Hello_World.go
@@ -0,0 +1,21 @@
+/*
+package main
+
+import "fmt"
+
+	func main() {
+		fmt.Println("Hello, World!")
+	}
+*/
+package main
+
+import "fmt"
+
+func main() {
+	var a string = "Hello"
+	var b string = "World"
+
+	var c string = a + " " + b
+
+	fmt.Println(c) // Output: "Hello World"
+}
